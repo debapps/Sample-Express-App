a56d0eed12c544a37b03f3ea73537769e685df86
Debaditya Bhar <debaditya.bhar@outlook.com>
2022-11-12 18:44:20 +0530

    Version 1.0 - Initial Version.
    This is a Express JS Web Application. This is used to install into AWS
    Cloud.

---
+++ app.js
@@ -0,0 +1,24 @@
+const express = require("express");
+
+// Create the express app.
+const app = express();
+
+// Setting the static folder.
+app.use(express.static("public"));
+
+// Route: /, Method: GET.
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/views/index.html", (err) => {
+    if (err) {
+      console.log(`Error in loading the html file content.`);
+      console.log(err);
+    }
+  });
+});
+
+// Define the port.
+const port = process.env.PORT || 3000;
+
+app.listen(port, () => {
+  console.log(`Application is listening on port http://localhost:${port}`);
+});
